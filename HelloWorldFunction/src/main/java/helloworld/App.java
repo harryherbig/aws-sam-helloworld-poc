1aa630404e72ba6e1620b656ee569a5c1a11462d
Harry Urban <hurban@stroeer.de>
2020-09-16 13:38:43 +0200

    return result as json to allow prelivehook to decide about alias shifting

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -3,20 +3,27 @@ package helloworld;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import org.json.JSONObject;
 
 /** Handler for requests to Lambda function. */
 public class App implements RequestHandler<SQSEvent, String> {
 
   public String handleRequest(final SQSEvent input, final Context context) {
-    final String fallbackReturn = "v1 Got empty input, can't parse record";
+    final JSONObject result =
+        new JSONObject().put("message", "Something went wrong!.").put("result", 500);
+    System.out.println(
+        "Hello World Function was invoked. My version is: "
+            + System.getenv("AWS_LAMBDA_FUNCTION_VERSION"));
     if (input.getRecords() != null) {
-      return input.getRecords().stream()
+      input.getRecords().stream()
           .filter(r -> r.getBody() != null)
           .findFirst()
-          .map(rec -> String.format("hello world v1, input was %s", rec.getBody()))
-          .orElse(fallbackReturn);
-    } else {
-      return fallbackReturn;
+          .ifPresent(
+              rec -> {
+                result.put("message", rec.getBody());
+                result.put("result", 200);
+              });
     }
+    return result.toString();
   }
 }
