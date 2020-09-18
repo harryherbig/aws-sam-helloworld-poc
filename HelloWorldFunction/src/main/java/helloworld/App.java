288e3a1c979235ce8bb51e625dbb14c5b81db2a5
Harry Urban <hurban@stroeer.de>
2020-09-18 22:34:25 +0200

    respond with json, because!

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -6,20 +6,24 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import org.json.JSONObject;
 
 /** Handler for requests to Lambda function. */
-public class App implements RequestHandler<SQSEvent, String> {
+public class App implements RequestHandler<SQSEvent, JSONObject> {
 
-  public String handleRequest(final SQSEvent input, final Context context) {
+  public JSONObject handleRequest(final SQSEvent input, final Context context) {
+    System.out.println(
+        "Hello World Function was invoked. My version is: "
+            + System.getenv("AWS_LAMBDA_FUNCTION_VERSION"));
     final JSONObject OK =
-        new JSONObject().put("message", "Harry says: All good in da hood!").put("statusCode", 200);
+        new JSONObject()
+            .put("message", "Harry says: All good in da hood!")
+            .put("statusCode", 200);
     final JSONObject BAD =
         new JSONObject()
             .put("message", "Harry forcing failure!.")
             .put("statusCode", 500);
-    System.out.println(
-        "Hello World Function was invoked. My version is: "
-            + System.getenv("AWS_LAMBDA_FUNCTION_VERSION"));
 
-//    return BAD.toString();
-    return OK.toString();
+    System.out.println("Will stop now and return my result");
+
+//    return BAD;
+    return OK;
   }
 }
